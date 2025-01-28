a8c0d39042e4535589274b6ad93a040ce71350c9
tl <99222342+tlapanco@users.noreply.github.com>
2025-01-28 15:21:46 -0600

    Add: SoftDeletes to Category

--- database/migrations/2025_01_10_213925_create_categories_table.php
+++ database/migrations/2025_01_10_213925_create_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name', length: 100);
             $table->timestamps();
+            $table->softDeletes('deleted_at', precision: 0);
         });
     }
 
